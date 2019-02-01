21158c527e90ba0cd40121e11464e439f2967d86
haubar <haubarclack@gmail.com>
2019-02-01 13:30:57 +0800

    update src source

--- injection.js
+++ injection.js
@@ -2,6 +2,6 @@ javascript:
       var timestamp = new Date().getTime();
           action      = document.createElement('script');
           action.type = 'text/javascript';
-          action.src  = 'http://localhost/alert.js';
+          action.src  = 'https://raw.githubusercontent.com/haubar/js-inject-tool/master/demo_alert.js';
           document.body.appendChild(action);
       void(0);
